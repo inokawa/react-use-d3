64bf38cff65ea1272f582d57f23543264e1bd51b
inokawa <48897392+inokawa@users.noreply.github.com>
2020-10-23 22:43:29 +0900

    fix

--- src/elem.tsx
+++ src/elem.tsx
@@ -10,62 +10,62 @@ type Props = {
   [key: string]: any;
 };
 
-export const Elem = React.memo(
-  ({ type, children, className, ...rest }: Props) => {
-    const ref = useRef<HTMLElement>(null);
+export const Elem = React.memo(({ type, children, ...props }: Props) => {
+  const ref = useRef<HTMLElement>(null);
 
-    const [props, functions] = Object.entries(rest).reduce<
-      [{ [key: string]: any }, { [key: string]: any }]
-    >(
-      (acc, [k, v]) => {
-        if (k.startsWith("on")) {
-          acc[1][k] = v;
-        } else {
-          acc[0][k] = v;
-        }
-
-        return acc;
-      },
-      [{}, {}]
-    );
-
-    useEffect(() => {
-      if (!ref.current) return;
-      if (typeof type !== "string") {
-        return;
+  const { className, ...rest } = props;
+  const [attrs, functions] = Object.entries(rest).reduce<
+    [{ [key: string]: any }, { [key: string]: any }]
+  >(
+    (acc, [k, v]) => {
+      if (k.startsWith("on")) {
+        acc[1][k] = v;
+      } else {
+        acc[0][k] = v;
       }
-      const el = d3.select(ref.current).data([props]);
-      // TODO enter
-      const enter = el.enter();
+      return acc;
+    },
+    [{}, {}]
+  );
 
-      // TODO update
-      const t = d3Transition.transition();
-      const update = enter.merge(el).transition(t);
-      Object.entries(props).forEach(([key, val]) => {
-        if (key === "style") {
-          for (const sKey of Object.keys(val)) {
-            update.style(kebabCase(sKey), val[sKey]);
-          }
-        } else if (typeof val === "function") {
-          if (key.startsWith("on")) {
-          } else {
-          }
-        } else {
-          update.attr(key, val);
+  useEffect(() => {
+    if (!ref.current) return;
+    if (typeof type !== "string") {
+      return;
+    }
+    const el = d3.select(ref.current).data([attrs]);
+    // TODO enter
+    const enter = el.enter();
+
+    // TODO update
+    const t = d3Transition.transition();
+    const update = enter.merge(el).transition(t);
+    Object.entries(attrs).forEach(([key, val]) => {
+      if (key === "style") {
+        for (const sKey of Object.keys(val)) {
+          update.style(kebabCase(sKey), val[sKey]);
         }
-      });
-      // TODO exit
-      const exit = el.exit();
-      exit.remove();
-    }, [props]);
+      } else if (typeof val === "function") {
+        // TODO
+      } else {
+        update.attr(key, val);
+      }
+    });
 
-    return React.createElement(
-      type,
-      { ...functions, className, ref },
-      createElem(children)
-    );
+    // TODO exit
+    const exit = el.exit();
+    exit.remove();
+  }, [attrs]);
+
+  if (typeof type !== "string") {
+    return React.createElement(type, props, createElem(children));
   }
-);
+  return React.createElement(
+    type,
+    { ...functions, className, ref },
+    createElem(children)
+  );
+});
 
 export const createElem = (children: React.ReactNode) => {
   if (!children || typeof children === "boolean") {
