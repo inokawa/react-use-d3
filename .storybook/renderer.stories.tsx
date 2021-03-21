240175d50710b884e82ef35b9969499b81ea6496
inokawa <48897392+inokawa@users.noreply.github.com>
2021-03-22 00:40:49 +0900

    Add story

---
+++ .storybook/renderer.stories.tsx
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from "react";
+import { Renderer } from "../src/renderer";
+import { getRangedData } from "./util";
+import * as d3 from "d3";
+
+export default {
+  title: "renderer",
+};
+
+export const Div = () => {
+  const [datas, setDatas] = useState<{ name: number; value: number }[]>(
+    getRangedData(1000)
+  );
+  useEffect(() => {
+    setInterval(() => {
+      setDatas(getRangedData(100));
+    }, 1000);
+  }, []);
+  const [focused, setFocused] = useState<number | null>(null);
+
+  return (
+    <Renderer>
+      {datas.map((d) => (
+        <div
+          key={d.name}
+          style={{
+            width: `${d.value}px`,
+            background: d.name === focused ? "orange" : "steelblue",
+          }}
+          onClick={() => alert(d.value)}
+          onMouseEnter={() => {
+            setFocused(d.name);
+          }}
+          onMouseLeave={() => {
+            setFocused(null);
+          }}
+        >
+          {d.value}
+        </div>
+      ))}
+    </Renderer>
+  );
+};
